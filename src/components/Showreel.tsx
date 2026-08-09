b525b10958d6583611b22e3bdf3f8102aea9627e
NadiiaZ <107829108+NadiiaZ@users.noreply.github.com>
2026-08-09 15:29:10 +0300

    Update Showreel.tsx

--- src/components/Showreel.tsx
+++ src/components/Showreel.tsx
@@ -49,6 +49,7 @@ export default function Showreel() {
               accent={frame.accent}
               showPlay
               ratioClassName="h-full w-full rounded-none"
+              video = "/media/work/ANB-12_v3_en_1080x1350.mp4"
             />
           </motion.div>
         </AnimatePresence>
